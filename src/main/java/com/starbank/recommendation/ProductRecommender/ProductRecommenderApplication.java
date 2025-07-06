f7bb9eb6f4bf07bbfd6b469531f94c222b05f553
lNasralnikl <raidar920357@gmail.com>
2025-07-06 13:09:10 +0300

    Initial commit

---
+++ src/main/java/com/starbank/recommendation/ProductRecommender/ProductRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.starbank.recommendation.ProductRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRecommenderApplication.class, args);
+	}
+
+}
