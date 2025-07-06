7bd7978656a708108a3131089bcbab76573f7d13
Frozi <Frozik6k@yandex.ru>
2025-07-06 14:05:14 +0500

    Модели создал

--- src/main/java/com/starbank/recommendation/ProductRecommender/model/Recommendation.java
+++ src/main/java/com/starbank/recommendation/ProductRecommender/model/Recommendation.java
@@ -1,4 +1,46 @@
 package com.starbank.recommendation.ProductRecommender.model;
 
+import java.util.Objects;
+
 public class Recommendation {
+
+    private String name;
+    private int id;
+    private String text;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Recommendation that = (Recommendation) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/starbank/recommendation/ProductRecommender/model/User.java
+++ src/main/java/com/starbank/recommendation/ProductRecommender/model/User.java
@@ -1,4 +1,41 @@
 package com.starbank.recommendation.ProductRecommender.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Objects;
+
 public class User {
+
+    @JsonProperty("user_id")
+    private int id;
+    private List<Recommendation> recommendations;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Recommendation> getRecommendations() {
+        return recommendations;
+    }
+
+    public void setRecommendations(List<Recommendation> recommendations) {
+        this.recommendations = recommendations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
